bfa1dd7158334b9144ce109dc79f54f241d57faf
ken7python <active.ken.315@gmail.com>
2026-04-17 15:23:27 +0900

    first commit.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const name = "KenCode"
+  return (
+    <div>
+      <h1>こんにちは, {name}!</h1>
+      <p>Reactの練習中です</p>
+    </div>
+  )
+}
+
+export default App
